213dd08f6997e1d50e74e5775248d31edd38666d
Agustín Herranz <agustin.herranz@gmail.com>
2018-12-02 18:43:38 +0100

    Solve day 1 challenge.

---
+++ 1/day1.py
@@ -0,0 +1,39 @@
+from itertools import cycle
+
+
+def get_values():
+    with open('./input', 'rt') as f:
+        values = f.readlines()
+    return [int(value) for value in values if value]
+
+
+def calibration(calibration_values, initial_frequency=0):
+    return sum(calibration_values, initial_frequency)
+
+
+def find_duplicate_frequency(calibration_values, initial_frequency=0):
+    frequencies_found = {initial_frequency}
+    frequency = initial_frequency
+    for value in cycle(calibration_values):
+        frequency += value
+        if frequency not in frequencies_found:
+            frequencies_found.add(frequency)
+        else:
+            break
+    return frequency
+
+
+def calibrate():
+    calibration_values = get_values()
+
+    # Parti 1
+    frequency = calibration(calibration_values)
+    print(f"Calibrate with frequency {frequency}")
+
+    # Part 2
+    dup_frequency = find_duplicate_frequency(calibration_values)
+    print(f"The first duplicated frequency is: {dup_frequency}")
+
+
+if __name__ == "__main__":
+    calibrate()
---
+++ 1/test_day1.py
@@ -0,0 +1,27 @@
+import pytest
+
+from day1 import calibration, find_duplicate_frequency
+
+
+@pytest.mark.parametrize("calibration_values,frequency", [
+    ([+1, -2, +3, +1], 3),
+    ([+1, +1, +1], 3),
+    ([+1, +1, -2], 0),
+    ([-1, -2, -3], -6),
+])
+def test_calibration(calibration_values, frequency):
+    result = calibration(calibration_values)
+
+    assert result == frequency
+
+@pytest.mark.parametrize("calibration_values,frequency", [
+    ([+1, -1], 0),
+    ([+3, +3, +4, -2, -4], 10),
+    ([-6, +3, +8, +5, -6], 5),
+    ([+7, +7, -2, -7, -4], 14),
+
+])
+def test_find_duplicate_frequency(calibration_values, frequency):
+    result = find_duplicate_frequency(calibration_values)
+
+    assert result == frequency
