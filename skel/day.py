3d219337010ffbc22304025c88857eefaedf8a1c
Agustín Herranz <agustin.herranz@gmail.com>
2018-12-04 20:05:38 +0100

    Add basic skeleton

---
+++ skel/day.py
@@ -0,0 +1,20 @@
+
+
+def get_data():
+    with open('./input', 'rt') as f:
+        values = f.readlines()
+    return values
+
+
+def solve():
+    data = get_data()
+
+    # Part 1
+    print(f"Part1 - ... is: {None}")
+
+    # Part 2
+    print(f"Part2 - ... is: {None}")
+
+
+if __name__ == "__main__":
+    solve()
