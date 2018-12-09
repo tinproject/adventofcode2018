624d7df0b6826cc0c1046d69a4963cb19eda329c
Agustín Herranz <agustin.herranz@gmail.com>
2018-12-09 21:48:11 +0100

    Solve part 1 of day 9

--- 9/day.py
+++ 9/day.py
@@ -1,3 +1,4 @@
+from itertools import cycle
 
 
 def get_data():
@@ -6,14 +7,56 @@ def get_data():
     return values
 
 
+def parse_data(data):
+    tokens = data.split(" ")
+    num_players = int(tokens[0].strip())
+    last_marble_points = int(tokens[6].strip())
+    return num_players, last_marble_points
+
+
+def calc_circular_index(playground, current_marble_index, delta):
+    playground_size = len(playground)
+    if playground_size == 1:
+        return 1
+    new_index = current_marble_index + delta
+    return new_index % len(playground)
+
+
+def get_highest_score(num_players, last_marble_points):
+    playground = [0]
+    current_marble_index = 0
+
+    players = cycle(range(1, num_players + 1))
+    player_scores = [0 for _ in range(num_players + 1)]
+
+    for marble in range(1, last_marble_points + 1):
+        player = next(players)
+
+        if marble % 23 == 0:
+            next_marble_index = calc_circular_index(playground, current_marble_index, -7)
+            marble_removed = playground.pop(next_marble_index)
+            player_scores[player] += marble
+            player_scores[player] += marble_removed
+        else:
+            next_marble_index = calc_circular_index(playground, current_marble_index, +2)
+            playground.insert(next_marble_index, marble)
+
+        current_marble_index = next_marble_index
+
+    return max(player_scores)
+
+
 def solve():
     data = get_data()
 
     # Part 1
-    print(f"Part1 - ... is: {None}")
+    num_players, last_marble_points = parse_data(data[0])
+    highest_score = get_highest_score(num_players, last_marble_points)
+    print(f"Part1 - The hights score in the game is: {highest_score}")
 
     # Part 2
-    print(f"Part2 - ... is: {None}")
+    highest_score = get_highest_score(num_players, last_marble_points * 100)
+    print(f"Part2 - The hights score in the game with 100 times marbles is: {highest_score}")
 
 
 if __name__ == "__main__":
--- 9/test_day.py
+++ 9/test_day.py
@@ -1,9 +1,49 @@
 import pytest
 
 
+from day import parse_data, get_highest_score, calc_circular_index
+
+
 @pytest.fixture()
 def example_data():
     example_input = """
+    412 players; last marble is worth 71646 points
     """
-
     return [line.strip() for line in example_input.splitlines() if line.strip()]
+
+
+def test_parse_data(example_data):
+    expected_players = 412
+    expected_last_marble_points = 71646
+
+    result = parse_data(example_data[0])
+
+    assert result == (expected_players, expected_last_marble_points)
+
+
+@pytest.mark.parametrize("players, last_marble_points, highest_score", [
+    (9, 25, 32),
+    (10, 1618, 8317),
+    (13, 7999, 146373),
+    (17, 1104, 2764),
+    (21, 6111, 54718),
+    (30, 5807, 37305),
+])
+def test_get_highest_score(players, last_marble_points, highest_score):
+
+    result = get_highest_score(players, last_marble_points)
+
+    assert result == highest_score
+
+
+@pytest.mark.parametrize("playground, current_marble_index, delta, index", [
+    ([0], 0, +2, 1),
+    ([0, 1], 1, +2, 1),
+    ([0, 2, 1, 3], 3, +2, 1),
+    ([0, 4, 2, 1, 3], 1, +2, 3),
+])
+def test_calc_circular_index(playground, current_marble_index, delta, index):
+
+    result = calc_circular_index(playground, current_marble_index, delta)
+
+    assert result == index
