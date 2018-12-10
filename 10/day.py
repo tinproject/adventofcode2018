c3f0ca9beac9dcfc57674ebb871ed6e74ac8754b
Agustín Herranz <agustin.herranz@gmail.com>
2018-12-10 23:17:29 +0100

    Solve part 1 of day 10.

--- 10/day.py
+++ 10/day.py
@@ -1,3 +1,45 @@
+from collections import namedtuple
+from dataclasses import dataclass
+from operator import itemgetter, attrgetter
+import re
+
+
+Velocity = namedtuple('Point', ['dx', 'dy'])
+
+
+@dataclass(eq=True, order=True)
+class Point:
+    x: int
+    y: int
+
+
+class LightPoint:
+    def __init__(self, origin, velocity):
+        self.origin = origin
+        self.velocity = velocity
+
+    _RE = re.compile(r".*position=<\s*(?P<x>-?\d+),\s+(?P<y>-?\d+).*velocity=<\s*(?P<dx>-?\d+),\s+(?P<dy>-?\d+).*$")
+
+    @classmethod
+    def from_input(cls, input_data):
+        m = cls._RE.match(input_data)
+        x = int(m.group('x'))
+        y = int(m.group('y'))
+        dx = int(m.group('dx'))
+        dy = int(m.group('dy'))
+        return cls(Point(x, y), Velocity(dx, dy))
+
+    def position_at_time(self, t):
+        x = self.origin.x + self.velocity.dx * t
+        y = self.origin.y + self.velocity.dy * t
+        return Point(x, y)
+
+    def __eq__(self, other):
+        return self.origin == other.origin and self.velocity == other.velocity
+
+    def __lt__(self, other):
+        # Not much sense physically
+        return self.origin < other.origin and self.velocity < other.velocity
 
 
 def get_data():
@@ -6,11 +48,67 @@ def get_data():
     return values
 
 
+def get_lightpoints(data):
+    lightpoints = []
+    for input_data in data:
+        lightpoints.append(LightPoint.from_input(input_data))
+    return lightpoints
+
+
+def get_points_at_ts(lightpoints, ts):
+    points = [lp.position_at_time(ts) for lp in lightpoints]
+
+    x_coords = [p.x for p in points]
+    y_coords = [p.y for p in points]
+    dx = abs(max(x_coords) - min(x_coords) + 1)
+    dy = abs(max(y_coords) - min(y_coords) + 1)
+
+    return points, dx, dy
+
+
+def get_message_ts(lightpoints):
+    variations = []
+    for ts in range(20000):
+        _, dx, dy = get_points_at_ts(lightpoints, ts)
+        if dy <= 50:
+            v = (dx, dy, ts)
+            variations.append(v)
+
+    message_ts = min(variations, key=itemgetter(1))[2]
+    return message_ts
+
+
+def get_sky_at_ts(ligthpoints, ts):
+    points, _, _ = get_points_at_ts(ligthpoints, ts)
+
+    min_x = min(points, key=attrgetter('x')).x - 1
+    max_x = max(points, key=attrgetter('x')).x + 1
+    dx = max_x - min_x + 1
+    min_y = min(points, key=attrgetter('y')).y - 1
+    max_y = max(points, key=attrgetter('y')).y + 1
+    dy = max_y - min_y + 1
+
+    print(dx, dy)
+
+    sky = []
+    for _ in range(dy):
+        sky.append(['_'] * dx)
+
+    for point in points:
+        sky[point.y - min_y][point.x - min_x] = "#"
+
+    for i, l in enumerate(sky):
+        sky[i] = "".join(l)
+        print(sky[i])
+
+
 def solve():
     data = get_data()
-
+    lightpoints = get_lightpoints(data)
     # Part 1
-    print(f"Part1 - ... is: {None}")
+    message_ts = get_message_ts(lightpoints)
+    print(f"Part1 - The message will show at time ts={message_ts}. The solution is... (read the message)")
+    get_sky_at_ts(lightpoints, message_ts)
 
     # Part 2
     print(f"Part2 - ... is: {None}")
--- 10/test_day.py
+++ 10/test_day.py
@@ -1,9 +1,103 @@
 import pytest
 
 
+from day import Point, Velocity, LightPoint, get_lightpoints, get_points_at_ts, get_message_ts
+
+
 @pytest.fixture()
 def example_data():
     example_input = """
+    position=< 9,  1> velocity=< 0,  2>
+    position=< 7,  0> velocity=<-1,  0>
+    position=< 3, -2> velocity=<-1,  1>
+    position=< 6, 10> velocity=<-2, -1>
+    position=< 2, -4> velocity=< 2,  2>
+    position=<-6, 10> velocity=< 2, -2>
+    position=< 1,  8> velocity=< 1, -1>
+    position=< 1,  7> velocity=< 1,  0>
+    position=<-3, 11> velocity=< 1, -2>
+    position=< 7,  6> velocity=<-1, -1>
+    position=<-2,  3> velocity=< 1,  0>
+    position=<-4,  3> velocity=< 2,  0>
+    position=<10, -3> velocity=<-1,  1>
+    position=< 5, 11> velocity=< 1, -2>
+    position=< 4,  7> velocity=< 0, -1>
+    position=< 8, -2> velocity=< 0,  1>
+    position=<15,  0> velocity=<-2,  0>
+    position=< 1,  6> velocity=< 1,  0>
+    position=< 8,  9> velocity=< 0, -1>
+    position=< 3,  3> velocity=<-1,  1>
+    position=< 0,  5> velocity=< 0, -1>
+    position=<-2,  2> velocity=< 2,  0>
+    position=< 5, -2> velocity=< 1,  2>
+    position=< 1,  4> velocity=< 2,  1>
+    position=<-2,  7> velocity=< 2, -2>
+    position=< 3,  6> velocity=<-1, -1>
+    position=< 5,  0> velocity=< 1,  0>
+    position=<-6,  0> velocity=< 2,  0>
+    position=< 5,  9> velocity=< 1, -2>
+    position=<14,  7> velocity=<-2,  0>
+    position=<-3,  6> velocity=< 2, -1>
     """
 
     return [line.strip() for line in example_input.splitlines() if line.strip()]
+
+
+def test_lightpoint_from_input():
+    input_data = "position=< 9,  1> velocity=< 0,  2>"
+    expected_point = Point(9, 1)
+    expected_velocity = Velocity(0, 2)
+
+    result = LightPoint.from_input(input_data)
+
+    assert result.origin == expected_point
+    assert result.velocity == expected_velocity
+
+
+def test_lightpoint_position_at_t0():
+    input_data = "position=< 9,  1> velocity=< 0,  2>"
+    ts = 0
+
+    lp = LightPoint.from_input(input_data)
+
+    result = lp.position_at_time(ts)
+
+    assert result == lp.origin
+
+
+def test_get_lightpoints(example_data):
+    lp = LightPoint(Point(-2, 2), Velocity(2, 0))
+
+    result = get_lightpoints(example_data)
+
+    assert lp in result
+
+
+def test_get_points_at_ts(example_data):
+    lightpoints = get_lightpoints(example_data)
+    ts = 0
+    point = Point(-2, 2)
+
+    result, _, _ = get_points_at_ts(lightpoints, ts)
+
+    assert point in result
+
+
+def test_get_points_at_ts_boundary(example_data):
+    lightpoints = get_lightpoints(example_data)
+    ts = 3
+    expected_dx = 10
+    expected_dy = 8
+
+    _, dx, dy = get_points_at_ts(lightpoints, ts)
+
+    assert (dx, dy) == (expected_dx, expected_dy)
+
+
+def test_get_message_ts(example_data):
+    lightpoints = get_lightpoints(example_data)
+    expected_message_ts = 3
+
+    result = get_message_ts(lightpoints)
+
+    assert result == expected_message_ts
