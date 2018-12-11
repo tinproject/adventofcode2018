e96579f671f5081787d0dbf74b0b37ad25580780
Agustín Herranz <agustin.herranz@gmail.com>
2018-12-11 23:44:40 +0100

    Solve part2 of day 11, add output.

--- 11/day.py
+++ 11/day.py
@@ -3,11 +3,7 @@ from itertools import product
 from operator import itemgetter
 
 
-@lru_cache(maxsize=None)
-def get_power_level(serial_number, x, y):
-    rack_id = x + 10
-    power_level = ((((rack_id * y + serial_number) * rack_id) // 100) % 10) - 5
-    return power_level
+GRID_SIZE = 300
 
 
 def get_data():
@@ -16,31 +12,58 @@ def get_data():
     return values[0]
 
 
-def get_total_power(serial_number, x, y):
-    total_power = sum(get_power_level(serial_number, i, j) for i in range(x, x+3) for j in range(y, y+3))
+@lru_cache(maxsize=2 ** 22)
+def get_total_power(serial_number, x, y, area_side=3):
+    if area_side == 1:
+        rack_id = x + 10
+        total_power = ((((rack_id * y + serial_number) * rack_id) // 100) % 10) - 5
+    elif 1 < area_side <= GRID_SIZE and area_side % 2 != 0:
+        total_power = get_total_power(serial_number, x, y, area_side-1)
+        for i in range(x, x+area_side):
+            total_power += get_total_power(serial_number, i, y+area_side-1, 1)
+        for j in range(y, y+area_side-1):
+            total_power += get_total_power(serial_number, x+area_side-1, j, 1)
+        return total_power
+    elif 1 < area_side <= GRID_SIZE and area_side % 2 == 0:
+        middle_side = area_side // 2
+        total_power = get_total_power(serial_number, x, y, middle_side)
+        total_power += get_total_power(serial_number, x+middle_side, y, middle_side)
+        total_power += get_total_power(serial_number, x, y+middle_side, middle_side)
+        total_power += get_total_power(serial_number, x+middle_side, y+middle_side, middle_side)
+    else:
+        raise ValueError
     return total_power
 
 
-def get_max_power_area(serial_number):
-    grid_side = 300
-
+def get_max_power_area_for_size(serial_number, area_side=3):
     power_areas = []
-    for y, x in product(range(1, grid_side-2), range(1, grid_side-2)):
-        power_areas.append((x, y, get_total_power(serial_number, x, y)))
+    for y, x in product(range(1, GRID_SIZE-area_side+2), range(1, GRID_SIZE-area_side+2)):
+        power_areas.append((x, y, area_side, get_total_power(serial_number, x, y, area_side)))
 
-    return max(power_areas, key=itemgetter(2))
+    return max(power_areas, key=itemgetter(3))
+
+
+def get_max_power_area(serial_number):
+    max_power_areas = []
+
+    for area_side in range(1, GRID_SIZE+1):
+        max_power_areas.append(get_max_power_area_for_size(serial_number, area_side))
+
+    return max(max_power_areas, key=itemgetter(3))
 
 
 def solve():
     data = get_data()
 
     # Part 1
-    max_power_area = get_max_power_area(data)
-    print(f"Part1 - The largest total power is {max_power_area[2]} for "
+    max_power_area = get_max_power_area_for_size(data)
+    print(f"Part1 - The largest total power is {max_power_area[3]} for "
           f"top-left corner: {max_power_area[0]},{max_power_area[1]}")
 
     # Part 2
-    print(f"Part2 - ... is: {None}")
+    max_power_area = get_max_power_area(data)
+    print(f"Part1 - The largest total power is {max_power_area[3]} for "
+          f"top-left corner/size: {max_power_area[0]},{max_power_area[1]},{max_power_area[2]}")
 
 
 if __name__ == "__main__":
--- 11/test_day.py
+++ 11/test_day.py
@@ -1,6 +1,6 @@
 import pytest
 
-from day import get_power_level, get_total_power, get_max_power_area
+from day import get_total_power, get_max_power_area_for_size, get_max_power_area
 
 
 @pytest.fixture()
@@ -11,34 +11,39 @@ def example_data():
     return [line.strip() for line in example_input.splitlines() if line.strip()]
 
 
-@pytest.mark.parametrize("x, y, serial_number, power_level", [
-    (3, 5, 8, 4),
-    (122, 79, 57, -5),
-    (217, 196, 39, 0),
-    (101, 153, 71, 4),
+@pytest.mark.parametrize("x, y, area_side, serial_number, total_power_level", [
+    (3, 5, 1, 8, 4),
+    (122, 79, 1, 57, -5),
+    (217, 196, 1, 39, 0),
+    (101, 153, 1, 71, 4),
+    (33, 45, 3, 18, 29),
+    (21, 61, 3, 42, 30),
+    (90, 269, 16, 18, 113),
+    (232, 251, 12, 42, 119),
 ])
-def test_get_power_level(serial_number, x, y, power_level):
+def test_get_total_power(x, y, area_side, serial_number, total_power_level):
 
-    result = get_power_level(serial_number, x, y)
-
-    assert result == power_level
-
-
-@pytest.mark.parametrize("x, y, serial_number, total_power_level", [
-    (33, 45, 18, 29),
-    (21, 61, 42, 30),
-])
-def test_get_total_power(x, y, serial_number, total_power_level):
-
-    result = get_total_power(serial_number, x, y)
+    result = get_total_power(serial_number, x, y, area_side)
 
     assert result == total_power_level
 
 
-def test_get_max_power_area():
+def test_get_max_power_area_for_size():
     serial_number = 18
-    expected_top_left_fuel_cell = (33, 45, 29)
+    area_side = 3
+    expected_top_left_fuel_cell = (33, 45, 3, 29)
+
+    result = get_max_power_area_for_size(serial_number, area_side)
+
+    assert result == expected_top_left_fuel_cell
+
+
+@pytest.mark.parametrize("x, y, area_side, serial_number, total_power_level", [
+    (90, 269, 16, 18, 113),
+    (232, 251, 12, 42, 119),
+])
+def test_get_max_power_area(x, y, area_side, serial_number, total_power_level):
 
     result = get_max_power_area(serial_number)
 
-    assert result == expected_top_left_fuel_cell
+    assert result == (x, y, area_side, total_power_level)
