c6f28b341c431ed49a99bfdfeb41d9c9ac2fc042
Agustín Herranz <agustin.herranz@gmail.com>
2018-12-11 21:03:14 +0100

    Solve part 1 of day 11

--- 11/day.py
+++ 11/day.py
@@ -1,16 +1,43 @@
+from functools import lru_cache
+from itertools import product
+from operator import itemgetter
+
+
+@lru_cache(maxsize=None)
+def get_power_level(serial_number, x, y):
+    rack_id = x + 10
+    power_level = ((((rack_id * y + serial_number) * rack_id) // 100) % 10) - 5
+    return power_level
 
 
 def get_data():
     with open('./input', 'rt') as f:
-        values = [line.strip() for line in f.readlines()]
-    return values
+        values = [int(line.strip()) for line in f.readlines()]
+    return values[0]
+
+
+def get_total_power(serial_number, x, y):
+    total_power = sum(get_power_level(serial_number, i, j) for i in range(x, x+3) for j in range(y, y+3))
+    return total_power
+
+
+def get_max_power_area(serial_number):
+    grid_side = 300
+
+    power_areas = []
+    for y, x in product(range(1, grid_side-2), range(1, grid_side-2)):
+        power_areas.append((x, y, get_total_power(serial_number, x, y)))
+
+    return max(power_areas, key=itemgetter(2))
 
 
 def solve():
     data = get_data()
 
     # Part 1
-    print(f"Part1 - ... is: {None}")
+    max_power_area = get_max_power_area(data)
+    print(f"Part1 - The largest total power is {max_power_area[2]} for "
+          f"top-left corner: {max_power_area[0]},{max_power_area[1]}")
 
     # Part 2
     print(f"Part2 - ... is: {None}")
--- 11/test_day.py
+++ 11/test_day.py
@@ -1,5 +1,7 @@
 import pytest
 
+from day import get_power_level, get_total_power, get_max_power_area
+
 
 @pytest.fixture()
 def example_data():
@@ -7,3 +9,36 @@ def example_data():
     """
 
     return [line.strip() for line in example_input.splitlines() if line.strip()]
+
+
+@pytest.mark.parametrize("x, y, serial_number, power_level", [
+    (3, 5, 8, 4),
+    (122, 79, 57, -5),
+    (217, 196, 39, 0),
+    (101, 153, 71, 4),
+])
+def test_get_power_level(serial_number, x, y, power_level):
+
+    result = get_power_level(serial_number, x, y)
+
+    assert result == power_level
+
+
+@pytest.mark.parametrize("x, y, serial_number, total_power_level", [
+    (33, 45, 18, 29),
+    (21, 61, 42, 30),
+])
+def test_get_total_power(x, y, serial_number, total_power_level):
+
+    result = get_total_power(serial_number, x, y)
+
+    assert result == total_power_level
+
+
+def test_get_max_power_area():
+    serial_number = 18
+    expected_top_left_fuel_cell = (33, 45, 29)
+
+    result = get_max_power_area(serial_number)
+
+    assert result == expected_top_left_fuel_cell
