b381ede8fceffd475a9f5a13f24b5942089b7b0a
Agustín Herranz <agustin.herranz@gmail.com>
2018-12-19 23:45:27 +0100

    Day 17, part 1, in progress 2

--- 17/day.py
+++ 17/day.py
@@ -1,4 +1,4 @@
-from collections import deque
+from collections import Counter, deque
 from copy import copy
 from dataclasses import dataclass
 from operator import itemgetter, attrgetter
@@ -69,66 +69,132 @@ def parse_input(data):
 class Ground:
     def __init__(self, data, water_spring=Coord(500, 0)):
         self.ground = parse_input(data)
-        self.water_spring = water_spring
-        self.ground[water_spring] = WATER_SPRING
         coords = self.ground.keys()
         self.min = Coord(min(coords, key=attrgetter('x')).x, min(coords, key=attrgetter('y')).y)
         self.max = Coord(max(coords, key=attrgetter('x')).x, max(coords, key=attrgetter('y')).y)
+        self.water_spring = water_spring
+        self.ground[water_spring] = WATER_SPRING
+
+    def self_is(self, coord, *args):
+        return self[coord] in args
+
+    def up_is(self, coord, *args):
+        return self[coord.up()] in args
+
+    def down_is(self, coord, *args):
+        return self[coord.down()] in args
+
+    def left_is(self, coord, *args):
+        return self[coord.left()] in args
+
+    def right_is(self, coord, *args):
+        return self[coord.right()] in args
 
     def water_flow(self, start=None):
         if start is None:
             start: Coord = self.water_spring
 
-        current = start
-        for i in range(100):
-            current_square = self[current]
-            down = current.down()
-            bottom_square = self[down]
-            if current_square == STILL_WATER:
-                current = current.up()
-            elif bottom_square == SAND:
-                current = down
-                self[current] = FLOWING_WATER
-            elif bottom_square in (CLAY, STILL_WATER):
-                # print(f"Looking rigth from {current}")
-                coord = copy(current)
-                right_end = None
-                for x in range(current.x, self.max.x+1):
-                    right_coord = coord.right()
-                    right_square = self[right_coord]
-                    bottom_square = self[coord.down()]
-                    if right_square == SAND and bottom_square in (CLAY, STILL_WATER):
-                        coord = right_coord
-                        continue
-                    else:
-                        # found right end
-                        right_end = coord
-                        break
-                # print(f"Looking left from {current}")
-                coord = copy(current)
-                left_end = None
-                for x in range(current.x, self.min.x-1, -1):
-                    left_coord = coord.left()
-                    left_square = self[left_coord]
-                    bottom_square = self[coord.down()]
-                    if left_square == SAND and bottom_square in (CLAY, STILL_WATER):
-                        coord = left_coord
-                        continue
-                    else:
-                        # found right end
-                        left_end = coord
-                        break
+        flow_to_process = deque([start])
+        print(f"min {self.min} max {self.max}")
 
-                # print(f"Left {left_end} Right {right_end}")
-                if self[left_end.left()] == CLAY and self[right_end.right()] == CLAY:
-                    # Fill with water
-                    for x in range(left_end.x, right_end.x+1):
-                        self[Coord(x, left_end.y)] = STILL_WATER
-                else:
-                    # Flowing water
-                    for x in range(left_end.x, right_end.x+1):
-                        self[Coord(x, left_end.y)] = FLOWING_WATER
+        while flow_to_process:
+            current = flow_to_process.popleft()
+            # print(f"=======>>> Processing {current}")
 
+            # if self.self_is(current, WATER_SPRING):
+            #     print(f"==> Don't process water spring {current}")
+            #     continue
+
+            if self.self_is(current, FLOWING_WATER) and self.down_is(current, FLOWING_WATER):
+                # print(f"**** Ignore current point: {current}")
+                # print(f"****   {self[current.up()]}")
+                # print(f"****  {self[current.left()]}{self[current]}{self[current.right()]}")
+                # print(f"****   {self[current.down()]}")
+
+                continue
+
+            if self.self_is(current, STILL_WATER):
+                # print(f"==> Process current already drowned {current}")
+                while self.up_is(current, STILL_WATER):
+                    current = current.up()
+                if self.up_is(current, FLOWING_WATER):
+                    # print(f"==============>> Append to process up {current.up()}")
+                    flow_to_process.append(current.up())
+                continue
+
+            if self.down_is(current, SAND, FLOWING_WATER):
+                # print(f"==> Water flowing down in {current}")
+                down = current.down()
+
+                while self.down_is(down, SAND, FLOWING_WATER):
+                    # print(down)
+                    self[down] = FLOWING_WATER
+                    if down.y >= self.max.y:
+                        # print(f"Out of range!!! {down}")
+                        break
+                    down = down.down()
+                self[down] = FLOWING_WATER
+                if not down.y >= self.max.y:
+                    flow_to_process.append(down)
+                    # print(f"==============>> Append to process down {down}")
+                continue
+
+            if self.down_is(current, CLAY, STILL_WATER):
+                # print(f"==> Water flowing sides in {current}")
+                while self.down_is(current, CLAY, STILL_WATER):
+
+                    # print(f"Looking rigth from {current}")
+                    coord = copy(current)
+                    right_end = None
+                    for x in range(current.x, self.max.x+2):
+                        if self.right_is(coord, SAND, FLOWING_WATER) and self.down_is(coord, CLAY, STILL_WATER):
+                            coord = coord.right()
+                            continue
+                        else:
+                            # found right end
+                            right_end = coord
+                            break
+                    # print(f"Looking left from {current}")
+                    coord = copy(current)
+                    left_end = None
+                    for x in range(current.x, self.min.x-1, -1):
+                        if self.left_is(coord, SAND, FLOWING_WATER) and self.down_is(coord, CLAY, STILL_WATER):
+                            coord = coord.left()
+                            continue
+                        else:
+                            # found right end
+                            left_end = coord
+                            break
+
+                    # print(f"Water spans Left {left_end} {self[left_end]} Right {right_end} {self[right_end]}")
+                    # print(f"Water spans Left {left_end.left()} {self[left_end.left()]} Right {right_end.right()} {self[right_end.right()]}")
+
+                    if self[left_end.left()] == CLAY and self[right_end.right()] == CLAY:
+                        # print(f"Filling with water")
+                        for x in range(left_end.x, right_end.x+1):
+                            self[Coord(x, left_end.y)] = STILL_WATER
+                        current = current.up()
+                        # print(f"Up {current}")
+                    else:
+                        # print(f"Flowing water")
+                        for x in range(left_end.x, right_end.x+1):
+                            self[Coord(x, left_end.y)] = FLOWING_WATER
+
+                        if self[left_end.left()] == SAND:
+                            # print(f"==============>> Append to process left {left_end}")
+                            flow_to_process.append(left_end)
+                        if self[right_end.right()] == SAND:
+                            # print(f"==============>> Append to process right {right_end}")
+                            flow_to_process.append(right_end)
+                        break
+                continue
+
+            # print(f"**** Can't process current point: {current}")
+            # print(f"****   {self[current.up()]}")
+            # print(f"****  {self[current.left()]}{self[current]}{self[current.right()]}")
+            # print(f"****   {self[current.down()]}")
+
+            continue
 
     def __getitem__(self, item):
         return self.ground.get(item, SAND)
@@ -137,9 +203,14 @@ class Ground:
         self.ground[key] = value
 
     def __repr__(self):
-        lines = []
-        for y in range(50):  # self.max.y+1):
-            lines.append("".join(self[Coord(x, y)] for x in range(self.min.x-3, self.max.x+3+1)))
+        lines = [
+            "      " + "".join(str(x % 1000 // 100) for x in range(self.min.x-3, self.max.x+3 + 1)),
+            "      " + "".join(str(x % 100 // 10) for x in range(self.min.x-3, self.max.x+3 + 1)),
+            "      " + "".join(str(x % 10) for x in range(self.min.x-3, self.max.x+3 + 1)),
+        ]
+        # for y in range(60):  # self.max.y+1):
+        for y in range(self.max.y+10):
+            lines.append(f"L{y:04} " + "".join(self[Coord(x, y)] for x in range(self.min.x-3, self.max.x+3+1)))
         return "\n".join(lines)
 
 
@@ -149,8 +220,16 @@ def solve():
     # Part 1
     ground = Ground(data)
     ground.water_flow()
-    print(ground)
-    print(f"Part1 - ... is: {None}")
+    # try:
+    #     ground.water_flow()
+    # except Exception:
+    #     pass
+    ground_slice = repr(ground)
+    print(ground_slice)
+    print("")
+    c = Counter(ground_slice)
+    water_tiles = c[STILL_WATER] + c[FLOWING_WATER]
+    print(f"Part1 - The number of tiles the water can reach is: {water_tiles}")
 
     # Part 2
     print(f"Part2 - ... is: {None}")
