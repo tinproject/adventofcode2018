57b368452fc83a49bf0bf87a5da485061ddaa34b
Agustín Herranz <agustin.herranz@gmail.com>
2018-12-25 16:24:38 +0100

    Solve part 1 of day 22

---
+++ 22/day.py
@@ -0,0 +1,94 @@
+from itertools import product
+from functools import lru_cache
+
+
+MOUTH = 'M'
+TARGET = 'X'
+ROCKY = '.'
+WET = '='
+NARROW = '|'
+
+
+class Cave:
+    REGION_TYPE = {
+        0: ROCKY,
+        1: WET,
+        2: NARROW,
+    }
+    RISK_LEVEL = {
+        ROCKY: 0,
+        MOUTH: 0,
+        TARGET: 0,
+        WET: 1,
+        NARROW: 2,
+    }
+
+    def __init__(self, depth, target, mouth=(0, 0)):
+        self.depth = depth
+        self.target = target
+        self.mouth = mouth
+        self.tiles = {}
+        self[mouth] = MOUTH
+        self[target] = TARGET
+
+    @lru_cache()
+    def get_geological_index(self, x, y):
+        if (x, y) == self.mouth:
+            return 0
+        if (x, y) == self.target:
+            return 0
+        if y == 0:
+            return x * 16807
+        if x == 0:
+            return y * 48271
+
+        return self.get_erosion_level(x-1, y) * self.get_erosion_level(x, y-1)
+
+    @lru_cache()
+    def get_erosion_level(self, x, y):
+        erosion_level = (self.get_geological_index(x, y) + self.depth) % 20183
+        return erosion_level
+
+    @lru_cache()
+    def get_type(self, x, y):
+        erosion_level = self.get_erosion_level(x, y)
+        t = erosion_level % 3
+        return self.REGION_TYPE[t]
+
+    def get_risk_level(self):
+        risk_level = sum(self.RISK_LEVEL[self[(x, y)]] for y, x in product(
+            range(self.mouth[1], self.target[1]+1),
+            range(self.mouth[0], self.target[0]+1),
+        ))
+        return risk_level
+
+    def __setitem__(self, point, value):
+        self.tiles[point] = value
+
+    def __getitem__(self, point):
+        value = self.tiles.get(point, None)
+        if value is None:
+            value = self.get_type(*point)
+            self[point] = value
+        return value
+
+    def __repr__(self):
+        return ""
+
+
+def solve():
+    depth = 11820
+    target = (7, 782)
+
+    # Part 1
+    cave = Cave(depth, target)
+    print(cave)
+    risk_level = cave.get_risk_level()
+    print(f"Part1 - The risk level of the smalles rectangle that include the mouth and target is: {risk_level}")
+
+    # Part 2
+    print(f"Part2 - ... is: {None}")
+
+
+if __name__ == "__main__":
+    solve()
---
+++ 22/test_day.py
@@ -0,0 +1,67 @@
+import pytest
+
+
+from day import Cave, ROCKY, WET, NARROW
+
+
+@pytest.fixture()
+def example_cave():
+    depth = 510
+    mouth = 0, 0
+    target = 10, 10
+
+    cave = Cave(depth, target, mouth)
+
+    yield cave
+
+
+@pytest.mark.parametrize("x, y, expected_geological_index", [
+    (0, 0, 0),
+    (10, 10, 0),
+    (1, 0, 16807),
+    (10, 0, 168070),
+    (0, 1, 48271),
+    (0, 10, 482710),
+    (1, 1, 145722555),
+])
+def test_get_geological_index(x, y, expected_geological_index, example_cave):
+
+    result = example_cave.get_geological_index(x, y)
+
+    assert result == expected_geological_index
+
+
+@pytest.mark.parametrize("x, y, expected_erosion_level", [
+    (0, 0, 510),
+    (1, 0, 17317),
+    (0, 1, 8415),
+    (1, 1, 1805),
+    (10, 10, 510),
+])
+def test_get_erosion_level(x, y, expected_erosion_level, example_cave):
+
+    result = example_cave.get_erosion_level(x, y)
+
+    assert result == expected_erosion_level
+
+
+@pytest.mark.parametrize("x, y, expected_type", [
+    (0, 0, ROCKY),
+    (1, 0, WET),
+    (0, 1, ROCKY),
+    (1, 1, NARROW),
+    (10, 10, ROCKY),
+])
+def test_get_type(x, y, expected_type, example_cave):
+
+    result = example_cave.get_type(x, y)
+
+    assert result == expected_type
+
+
+def test_risk_level(example_cave):
+    expected_risk_level = 114
+
+    result = example_cave.get_risk_level()
+
+    assert result == expected_risk_level
